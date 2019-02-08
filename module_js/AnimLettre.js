ba8759adf5bf9232d6ce20bacdd97874a9754ff1
Nguyen GitHub <>
2019-02-08 16:59:24 -0500

    Commencer animation lettre

--- module_js/AnimLettre.js
+++ module_js/AnimLettre.js
@@ -2,3 +2,32 @@ import {
     Util
 } from './Util.js'
 
+export class AnimLettre {
+
+    constructor(s, elementHeader, finAnim) {
+        this.lesLettres = s.lesLettres;
+        this.elmHeader = elementHeader;
+        this.animLettre()
+        this.finAnim = finAnim
+
+    }
+
+    animLettre() {
+        let elmConteneur = this.creerElement(this.elmHeader,
+            'div',
+            this.lesLettres,
+            'mot')
+    }
+
+    creerLettre(elmParent, balise, contenu, classCss) {
+        console.log(balise)
+        let noeud = document.createElement(balise)
+        if (contenu != '') {
+            noeud.innerHTML = contenu
+        }
+        noeud.classList.add(classCSS)
+        elmParent.appendChild(noeud)
+        return noeud
+    }
+
+}
