463391dc987c682eb05226be2725b411c2e47cf4
Nguyen GitHub <>
2019-02-08 17:20:40 -0500

    Creation du texte

--- module_js/AnimLettre.js
+++ module_js/AnimLettre.js
@@ -4,22 +4,45 @@ import {
 
 export class AnimLettre {
 
-    constructor(s, elementHeader, finAnim) {
-        this.lesLettres = s.lesLettres;
-        this.elmHeader = elementHeader;
+    constructor(lesLettres, elementParent, finAnim) {
+        this.lesLettres = lesLettres;
+        this.elmParent = elementParent;
         this.animLettre()
         this.finAnim = finAnim
 
     }
 
     animLettre() {
-        let elmConteneur = this.creerElement(this.elmHeader,
+        console.log('introduction')
+        let phrase = this.lesLettres
+        console.log(phrase)
+        
+        let elmPrincipale = this.creerElement(this.elmParent,
             'div',
             this.lesLettres,
             'mot')
+
+            // let i = 0;
+            // //let ensembleDesLettre = document.querySelectorAll(".mot > div")
+            // let lesLettres = document.querySelectorAll(".mot:nth-child(1) > div")
+            // let nbLettres = lesLettres.length;
+            // //Quand la page est chargée, on attribue un délai cumulatif de 0.5s
+            // //pour débuter l'animation de chaque lettre du mot JOYEUX
+    
+            // for (let uneLettre of lesLettres){
+                
+            //     uneLettre.style.animationDelay = (i * 0.5) + "s";
+            //     uneLettre.style.color = tabCouleur[(i++)%7]
+                
+            // }
+        
+    
+            // //Quand l'animation de la dernière lettre du mot joyeux est terminée la fonction animerNoel est appelée	
+            // lesLettres[nbLettres - 1].addEventListener("animationend", animerNoel, false);
+            // console.log('lesLettres[nbLettres - 1]' + lesLettres[nbLettres - 1].innerHTML)
     }
 
-    creerLettre(elmParent, balise, contenu, classCss) {
+    creerElement(elmParent, balise, contenu, classCSS) {
         console.log(balise)
         let noeud = document.createElement(balise)
         if (contenu != '') {
