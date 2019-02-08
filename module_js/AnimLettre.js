24d47900f3cc5b82a72775d5e45a804442652290
Nguyen GitHub <>
2019-02-08 17:36:40 -0500

    Animation

--- module_js/AnimLettre.js
+++ module_js/AnimLettre.js
@@ -15,31 +15,13 @@ export class AnimLettre {
     animLettre() {
         console.log('introduction')
         let phrase = this.lesLettres
-        console.log(phrase)
-        
+
         let elmPrincipale = this.creerElement(this.elmParent,
             'div',
             this.lesLettres,
             'mot')
 
-            // let i = 0;
-            // //let ensembleDesLettre = document.querySelectorAll(".mot > div")
-            // let lesLettres = document.querySelectorAll(".mot:nth-child(1) > div")
-            // let nbLettres = lesLettres.length;
-            // //Quand la page est chargée, on attribue un délai cumulatif de 0.5s
-            // //pour débuter l'animation de chaque lettre du mot JOYEUX
-    
-            // for (let uneLettre of lesLettres){
-                
-            //     uneLettre.style.animationDelay = (i * 0.5) + "s";
-            //     uneLettre.style.color = tabCouleur[(i++)%7]
-                
-            // }
         
-    
-            // //Quand l'animation de la dernière lettre du mot joyeux est terminée la fonction animerNoel est appelée	
-            // lesLettres[nbLettres - 1].addEventListener("animationend", animerNoel, false);
-            // console.log('lesLettres[nbLettres - 1]' + lesLettres[nbLettres - 1].innerHTML)
     }
 
     creerElement(elmParent, balise, contenu, classCSS) {
